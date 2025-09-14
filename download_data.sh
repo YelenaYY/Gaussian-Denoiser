25b8a90cd5ea2d834ad70da8ac8ed52d8bc25e52
greasycat <131315c@gmail.com>
2025-09-14 15:08:12 -0400

    Add model and training for DnCNN-a

--- download_data.sh
+++ download_data.sh
@@ -6,14 +6,25 @@ if [ -x tar ]; then
   echo "tar not installed."
 fi
 
+if [ -x unzip ]; then
+  echo "unzip not installed."
+fi
+
 DATA=data/
 DATA1=BSD300.tgz
+DATA2=train400.zip
+DATA2_DIR=train400
 
-mkdir $DATA
+mkdir $DATA -p
 
 if ! [ -f $DATA1 ]; then
   curl -L https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz \
     -o $DATA1
 fi
 
-tar -xvzf $DATA1 -C $DATA
+tar -xzf $DATA1 -C $DATA
+
+mkdir $DATA/$DATA2_DIR -p
+unzip $DATA2 -d $DATA/$DATA2_DIR
+
+echo "Data downloaded and extracted to $DATA"
